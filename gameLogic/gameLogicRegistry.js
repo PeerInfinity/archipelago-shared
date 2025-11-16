bd8ee3549fdaba20cf85e9134d0811f31f40493e
PeerInfinity <163462+PeerInfinity@users.noreply.github.com>
2025-11-15 17:48:07 -0800

    fixing naming issues

--- gameLogic/gameLogicRegistry.js
+++ gameLogic/gameLogicRegistry.js
@@ -234,11 +234,11 @@ import * as dlcquestLogic from './dlcquest/dlcquestLogic.js';
 import { dlcquestStateModule } from './dlcquest/dlcquestLogic.js';
 import * as hkLogic from './hk/hkLogic.js';
 import { hkStateModule } from './hk/hkLogic.js';
-import * as hylics2Logic from './hylics_2/hylics2Logic.js';
-import { hylics2StateModule } from './hylics_2/hylics2Logic.js';
+import * as hylics2Logic from './hylics2/hylics2Logic.js';
+import { hylics2StateModule } from './hylics2/hylics2Logic.js';
 import * as inscryptionLogic from './inscryption/inscryptionLogic.js';
 import { inscryptionStateModule } from './inscryption/inscryptionLogic.js';
-import * as jak_and_daxter__the_precursor_legacyLogic from './jak_and_daxter__the_precursor_legacy/jak_and_daxter__the_precursor_legacyLogic.js';
+import * as jakanddaxterLogic from './jakanddaxter/jakanddaxterLogic.js';
 import { kh1Logic } from './kh1/kh1Logic.js';
 import { helperFunctions as kh2HelperFunctions } from './kh2/kh2Logic.js';
 import * as kdl3Logic from './kdl3/kdl3Logic.js';
@@ -280,7 +280,7 @@ import * as shiversLogic from './shivers/shiversLogic.js';
 import * as smz3Logic from './smz3/smz3Logic.js';
 import * as sc2Logic from './sc2/sc2Logic.js';
 import * as subnauticaLogic from './subnautica/subnauticaLogic.js';
-import { helperFunctions as smHelperFunctions } from './super_metroid/smLogic.js';
+import { helperFunctions as smHelperFunctions } from './sm/smLogic.js';
 import * as stardewValleyHelpers from './stardew_valley/helpers.js';
 import * as terrariaLogic from './terraria/terrariaLogic.js';
 import * as timespinnerLogic from './timespinner/timespinnerLogic.js';
@@ -385,7 +385,7 @@ const GAME_REGISTRY = {
   },
   'Jak and Daxter: The Precursor Legacy': {
     logicModule: genericLogic.genericStateModule,
-    helperFunctions: jak_and_daxter__the_precursor_legacyLogic.helperFunctions,
+    helperFunctions: jakanddaxterLogic.helperFunctions,
     worldClasses: ['JakAndDaxterWorld'],
     aliases: ['Jak and Daxter: The Precursor Legacy', 'Jak and Daxter']
   },
